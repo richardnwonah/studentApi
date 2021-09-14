ccc99a61828fd369f0e9d256171fc12cb55f4487
Richard nwonah <nwonahr@gmail.com>
2021-09-14 08:30:03 +0100

    added student model

---
+++ studentApi/Controllers/Models/Student.cs
@@ -0,0 +1,13 @@
+namespace studentApi.Controllers.Models
+{
+    public class Student
+    {
+     
+        public int StudentId { get; set; }
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string City {get; set;}
+        public string State {get; set;}
+        
+    }
+}
